1d25f2d8c71469f0772b71413529ea680b64907c
grammnixon <grammnixon1@gmail.com>
2019-09-28 18:24:31 -0400

    Change js to load JSON externally

--- script.js
+++ script.js
@@ -1,98 +1,107 @@
-//JSON Blob representing API call
-var jsonVar = [
-    {
-        "id":1,
-        "name":"John Smith",
-        "position":"Server",
-        "applied":"03/15/16",
-        "experience":2,
-        "availability":{
-          "M":2,
-          "T":2,
-          "W":1,
-              "Th":2,
-              "F":1,
-              "S":0,
-              "Su":0
-           },
-           "questions":[
-              {
-                 "text":"Have you ever been convicted of a felony?",
-                 "answer":"No"
-              }
-           ]
-        },
-        {
-           "id":2,
-           "name":"Jane Smith",
-           "position":"Cook",
-           "applied":"02/08/16",
-           "experience":4,
-           "availability":{
-              "M":1,
-              "T":1,
-              "W":1,
-              "Th":1,
-              "F":0,
-              "S":0,
-              "Su":0
-           },
-           "questions":[
-              {
-                 "text":"Have you ever been convicted of a felony?",
-                 "answer":"Yes"
-              }
-           ]
-        },
-        {
-           "id":3,
-           "name":"David Jessup",
-           "position":"Chef",
-           "applied":"03/08/16",
-           "experience":2,
-           "availability":{
-              "M":2,
-              "T":2,
-              "W":2,
-              "Th":2,
-              "F":2,
-              "S":0,
-              "Su":0
-           },
-           "questions":[
-              {
-                 "text":"Are you authorized to work in the United States?",
-                 "answer":"Yes"
-              }
-           ]
-        },
-        {
-           "id":4,
-           "name":"Clay vanSchalkwijk",
-           "position":"Cook",
-           "applied":"03/08/16",
-           "experience":1,
-           "availability":{
-              "M":1,
-              "T":0,
-              "W":1,
-              "Th":0,
-              "F":1,
-              "S":0,
-              "Su":0
-           },
-           "questions":[
-              {
-                 "text":"Are you authorized to work in the United States?",
-                 "answer":"Yes"
-              },
-              {
-                "text":"Did you just buy a Subaru Crosstrek?",
-                "answer":"Yes"
-             }
-           ]
-        }
-];
+// //JSON Blob representing API call
+// var jsonVar = [
+//     {
+//         "id":1,
+//         "name":"John Smith",
+//         "position":"Server",
+//         "applied":"03/15/16",
+//         "experience":2,
+//         "availability":{
+//           "M":2,
+//           "T":2,
+//           "W":1,
+//               "Th":2,
+//               "F":1,
+//               "S":0,
+//               "Su":0
+//            },
+//            "questions":[
+//               {
+//                  "text":"Have you ever been convicted of a felony?",
+//                  "answer":"No"
+//               }
+//            ]
+//         },
+//         {
+//            "id":2,
+//            "name":"Jane Smith",
+//            "position":"Cook",
+//            "applied":"02/08/16",
+//            "experience":4,
+//            "availability":{
+//               "M":1,
+//               "T":1,
+//               "W":1,
+//               "Th":1,
+//               "F":0,
+//               "S":0,
+//               "Su":0
+//            },
+//            "questions":[
+//               {
+//                  "text":"Have you ever been convicted of a felony?",
+//                  "answer":"Yes"
+//               }
+//            ]
+//         },
+//         {
+//            "id":3,
+//            "name":"David Jessup",
+//            "position":"Chef",
+//            "applied":"03/08/16",
+//            "experience":2,
+//            "availability":{
+//               "M":2,
+//               "T":2,
+//               "W":2,
+//               "Th":2,
+//               "F":2,
+//               "S":0,
+//               "Su":0
+//            },
+//            "questions":[
+//               {
+//                  "text":"Are you authorized to work in the United States?",
+//                  "answer":"Yes"
+//               }
+//            ]
+//         },
+//         {
+//            "id":4,
+//            "name":"Clay vanSchalkwijk",
+//            "position":"Cook",
+//            "applied":"03/08/16",
+//            "experience":1,
+//            "availability":{
+//               "M":1,
+//               "T":0,
+//               "W":1,
+//               "Th":0,
+//               "F":1,
+//               "S":0,
+//               "Su":0
+//            },
+//            "questions":[
+//               {
+//                  "text":"Are you authorized to work in the United States?",
+//                  "answer":"Yes"
+//               },
+//               {
+//                 "text":"Did you just buy a Subaru Crosstrek?",
+//                 "answer":"Yes"
+//              }
+//            ]
+//         }
+// ];
+
+function Get(url){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",url,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
+
+var jsonVar = JSON.parse(Get("http://localhost:3000"));
  
 $('#search').keyup(function (){
     $('.card').removeClass('d-none'); //reset the search, removing d-none shows all cards again
